548cde02ba41c3b439835bcbccbf8c461715fe23
probasse <pl.kasee@gmail.com>
2017-01-21 07:55:13 +0800

    Enabling the router

--- webapp/Component.js
+++ webapp/Component.js
@@ -10,16 +10,11 @@ sap.ui.define([
 		metadata: {
 			manifest: "json"
 		},
-
-		/**
-		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-		 * @public
-		 * @override
-		 */
 		init: function() {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
-
+			// Create the views/URL based on hash
+			this.getRouter().initialize();
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
 		}
